1b9858589d377979621e6cb55d3c300c9711bfde
Olivier Mungo <olivier@mungo.eu>
2021-03-19 00:02:02 +0100

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Switch,
+  Route,
+  useHistory,
+  Redirect
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import { AppHeader } from './core'
+import { Board } from './board';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyA_Jq10AgfGrLwlZQW9qF4xUXWZxWUx4_Q",
+  authDomain: "planning-poker-test-70393.firebaseapp.com",
+  databaseURL: "https://planning-poker-test-70393-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "planning-poker-test-70393",
+  storageBucket: "planning-poker-test-70393.appspot.com",
+  messagingSenderId: "302069956118",
+  appId: "1:302069956118:web:28e225bdec917d15e6bf37",
+  measurementId: "G-ZL68D26ZK3"
+};
 
 function App() {
+  // Initialize Firebase
+  if (!firebase.apps.length) {
+    const database = firebase.initializeApp(firebaseConfig).database();
+    const ref = database.ref('boards');
+
+    ref.once('value').then(val => val.forEach(x => console.log(x.key, x.val())));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+
+      <Switch>
+        <Route path='/board'>
+          <Board />
+        </Route>
+        <Route path='/pig'>
+        </Route>
+        <Route path='/'>
+          <Redirect to='/board' />
+        </Route>
+      </Switch>
     </div>
   );
 }
