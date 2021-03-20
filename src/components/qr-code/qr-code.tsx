259826a9faf58ca7fb0a6ef4a6ff3f224a83b464
Olivier Mungo <olivier@mungo.eu>
2021-03-21 00:44:52 +0100

    Allow to send a mail with the URL to serve a pig

--- src/components/qr-code/qr-code.tsx
+++ src/components/qr-code/qr-code.tsx
@@ -1,17 +1,32 @@
+import './qr-code.css';
 import { QRCode } from "react-qr-svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 type Props = { value: string };
 
 export function QrCode(props: Props) {
     const { value } = props;
 
+    const handleClick = () => {
+        const subject = 'Your Planning Poker session is ready';
+        const message = value;
+        window.open('mailto:?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(message));
+    }
+
     return (
-        <QRCode
-            bgColor="#FFFFFF"
-            fgColor="#000000"
-            level="Q"
-            style={{ width: 256 }}
-            value={value}
-        />
+        <div className="qr-code">
+            <QRCode
+                bgColor="#FFFFFF"
+                fgColor="#000000"
+                level="Q"
+                style={{ width: 256 }}
+                value={value}
+            />
+
+            <button className="qr-code--button" onClick={handleClick}>
+                <FontAwesomeIcon className="card--icon" icon={faPaperPlane} />
+            </button>
+        </div>
     );
 }
