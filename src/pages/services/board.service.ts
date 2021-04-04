92a5c105f1eece6ee5374239cda2facbd41e2649
Olivier Mungo <olivier@mungo.eu>
2021-04-04 20:09:02 +0200

    Improve the locking mechanism

--- src/pages/services/board.service.ts
+++ src/pages/services/board.service.ts
@@ -18,14 +18,16 @@ export function checkBoardExists(boardKey: string): Promise<boolean> {
         .then(value => value.val() ? true : false);
 }
 
-export function getLock(boardKey: string): string | null {
-    return firebase.database().ref(`boards/${boardKey}/lock`).push().key;
+export function getLock(boardKey: string) {
+    const lock = window.localStorage.getItem('lock') || '';
+
+    if (!lock) {
+        const key = firebase.database().ref(`boards/${boardKey}/lock`).push().key;
+        window.localStorage.setItem('lock', key || '');
+    }
 }
 
-export function lockBoard(boardKey: string, key: string): Promise<any> {
-    if (window.location.hostname === 'localhost') {
-        return new Promise((resolve) => resolve(1));
-    } else {
-        return firebase.database().ref(`boards/${boardKey}/lock`).set({ timeStamp: new Date().getTime(), key });
-    }
+export function lockBoard(boardKey: string): Promise<any> {
+    const key = window.localStorage.getItem('lock') || '';
+    return firebase.database().ref(`boards/${boardKey}/lock`).set({ timeStamp: new Date().getTime(), key });
 }
